f368e3faa4e838e10e95aad14698ac29d412ed70
Jie Yuan <jie.yuan@microsoft.com>
2020-02-28 17:31:26 +0100

    Update MeetingCreation.ps1

--- MeetingCreation.ps1
+++ MeetingCreation.ps1
@@ -1,13 +1,15 @@
-# Azure AD OAuth User Token for Graph API
-# Get OAuth token for a AAD User (returned as $token)
+
+$graphUrl = 'https://graph.microsoft.com'
+$tenantId = "42d4a46d-9bc5-454b-821c-b1610ac5de9b"
 
 # Add required assemblies
-Add-Type -AssemblyName System.Web, PresentationFramework, PresentationCore
-
-Function GetToken{
+#Function GetToken
+#{
+	# Azure AD OAuth User Token for Graph API
+	# Get OAuth token for a AAD User (returned as $token)
+	Add-Type -AssemblyName System.Web, PresentationFramework, PresentationCore
 	# Application (client) ID, tenant ID and redirect URI
 	$clientId = "0ba51b74-369c-4494-8d48-0b4041a96f0c"
-	$tenantId = "42d4a46d-9bc5-454b-821c-b1610ac5de9b"
 	$clientSecret = 'H8I=A4We5:zKkXm0GslbH@YprlT-?eVY'
 	$redirectUri = "https://login.microsoftonline.com/common/oauth2/nativeclient"
 
@@ -25,7 +27,6 @@ Function GetToken{
 
 	# Construct URI
 	$uri = "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/authorize?client_id=$clientId&response_type=code&redirect_uri=$redirectUriEncoded&response_mode=query&scope=$scopeEncoded&state=$state"
-	$graphUrl = 'https://graph.microsoft.com'
 
 	# Create Window for User Sign-In
 	$windowProperty = @{
@@ -73,8 +74,7 @@ Function GetToken{
 	$authCode = $script:urlQueryValues.GetValues(($script:urlQueryValues.keys | Where-Object { $_ -eq "code" }))
 	
 	
-	if ($authCode) {
-
+	
 	    # With Auth Code, start getting token
 
 	    # Construct URI
@@ -93,75 +93,87 @@ Function GetToken{
 	    # Get OAuth 2.0 Token
 	    $tokenRequest = Invoke-WebRequest -Method Post -Uri $uri -ContentType "application/x-www-form-urlencoded" -Body $body
 
-	    # Access Token
-	    $token = ($tokenRequest.Content | ConvertFrom-Json).access_token
-	    return $token
-	}
-	else {
-	    Write-Error "Unable to obtain Auth Code!"
-	}
-}
+		# Access Token
+		$token = ($tokenRequest.Content | ConvertFrom-Json).access_token
+	    #return ($tokenRequest.Content | ConvertFrom-Json).access_token
 
-Function CreateMeeting{
-	param ($code, $subject, $listAttendees, $token)
-	
-	#build body
-	$meetingparam = '
-	{
-		"startDateTime":"2020-03-01T14:30:34.2444915-07:00",
-		"endDateTime":"2020-06-30T15:00:34.2464912-07:00",
-		"subject":"PS Graph Meeting",
-		"participants": {
+#}
 
-			"attendees": [
-				{
-					"identity":{
-						"user":{
-							"displayname":"$name",
-							"id":"$iduser"
-						}
-					},
-					"upn":"$upn"
-				}
-			]
-		}
-	}'
+# Code starts here _____________________________________________
+#$token = GetToken
 
-
-	$queryUrl = $graphUrl + "/v1.0/me/onlineMeetings"
-	
-	$response = Invoke-RestMethod -Method Post -Uri $queryUrl -ContentType 'application/json' -Headers @{Authorization = "Bearer $token"} -Body $meetingparam
-
-	$result = Invoke-RestMethod -Headers $authHeader -Uri $apiUrl -Body $body -Method Post -ContentType 'application/json'
-
-	$queryUrlUsers = $graphUrl + "/v1.0/me"
-
-	$user = Invoke-WebRequest -Method Get -Uri $queryUrlUsers -ContentType "application/json" -Headers @{Authorization = "Bearer $token"} -ErrorAction Stop
-	
-	return
-}
-
-Function GenerateAttendeeList{
-	
-}
-
-$token = GetToken();
-
-$csvOut = "meetingsoutput.csv"
+$csvOut = "C:\temp\meetingsoutput.csv"
 
 # Get input CSV and scan line by line
-$csv = Import-Csv ""
-foreach ($line in $file)
+$csv = Get-Content "C:\temp\InputCorsi.csv"
+
+foreach ($line in $csv)
 {
 	$line.Read | out-host
-	$lineValues = ([string]$file[$i]).Split(',').Trim()
+	return $line
+	$line='cclass1,Docente_Incarico_cclass1,alexw@M365EDU702432.onmicrosoft.com,fatimaz@M365EDU702432.onmicrosoft.com'
+	$lineValues = $line.Split(',').Trim()
 
 	$code = $lineValues[0]
 	$subject = $lineValues[1]
 	$listAttendees = $lineValues[2..($lineValues.Length-1)]
 	
 	# Create meeting and output generation: c_corso,subject,meeting id,join link,meeting options url
-	$meetingOutput = CreateMeeting($code, $subject, $listAttendees, $token)
+
+	$meetingbody = '
+    {
+		"startDateTime":"2020-03-01T14:30:34.2444915-07:00",
+		"endDateTime":"2020-06-30T16:00:34.2464912-07:00",
+        "subject":"' + $subject + '",
+        "participants": {
+            "attendees": [
+				{
+					"identity":{
+						"user":{
+							"displayname":"Adele Vance",
+							"id":"c134eca6-67d6-4bec-a4f3-21a1fd0fd4b8"
+						}
+					},
+					"upn":"AdeleV@M365EDU702432.OnMicrosoft.com"
+				}
+				'
+				
+    foreach ($attendee in $listAttendees) {
+
+        $getUserQueryUrl = $graphUrl + "/v1.0/users/$attendee"
+		$user = Invoke-WebRequest -Method Get -Uri $getUserQueryUrl -ContentType "application/json" -Headers @{Authorization = "Bearer $token"} | ConvertFrom-Json
+
+        $meetingbody += '     
+			,{
+				"identity":
+				{
+					"user":
+					{
+						"displayName": "'+$user.displayName+'",
+						"id": "'+$user.id+'"
+					}
+				},
+				"upn": "'+$user.userPrincipalName+'"
+			}
+		'
+	}
+	
+	$meetingbody+='
+		]
+		}
+	}'
+
+	# Create meeting Invok
+	$meetingQueryUrl = $graphUrl + "/v1.0/me/onlineMeetings"
+	$response = Invoke-RestMethod -Method Post -Uri $meetingQueryUrl -ContentType 'application/json' -Headers @{Authorization = "Bearer $token"} -Body $meetingbody
+
+	$meetingOrganizer = $response.participants.organizer.identity.user.id
+	$meetingCode = $response.chatInfo.threadId.Replace(":meeting","_meeting")
+
+    $meetingOptionUrl = "https://teams.microsoft.com/meetingOptions/?organizerId=" + $meetingOrganizer + "&tenantId=" + $tenantId + "&threadId=" + $meetingCode + "&messageId=0&language=en-US"
+
+	# c_corso,subject,meeting id,join link,meeting options url
+	$meetingOutput = $code + "," + $subject + "," + $response.id + "," + $response.joinWebUrl + "," + $meetingOptionUrl
 	
 	# Write output
 	Add-content $csvOut -value $meetingOutput
